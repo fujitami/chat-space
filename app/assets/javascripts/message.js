80dc9b787715fb2b76d88face2a5dfc3e872d657
fujitami <midori.fujita.0423@gmail.com>
2018-07-14 18:53:28 +0900

    Update message.js
    JSONでリクエストが出された時の処理、およびその際に使用するHTMLの記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,46 @@
 $(function(){
-  $('#form__content').on('submit', function(e) {
+  function buildHTML(message){
+    var img = message.image_url ? `<img class="lower-message__image" src="${ message.image_url }">` : ``
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${ message.user_name }
+                    </div>
+                    <div class="upper-message__date">
+                      ${ message.date }
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${ message.content }
+                    </p>
+                    ${ img }
+                  </div>
+                </div>
+              </div>`
+    return html;
+  }
+  $('#form__content').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
       url: url,
-      type: POST,
+      type: 'POST',
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+      $('.form__submit').prop('disabled', false);
+      $('.messages').animate({ scrollTop: $('.message:last').offset().top }, 'swing' );
+    })
+    .fail(function(){
+      alert('投稿に失敗しました。');
     })
   })
 })
